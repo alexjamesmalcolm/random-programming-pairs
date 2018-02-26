6d57a14234f9f7b59ec32321d1d63aa35295b927
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-02-26 12:00:15 -0500

    shouldHaveRandomOrder

--- src/main/java/com/alexjamesmalcolm/randomprogrammingpairs/PairGenerator.java
+++ src/main/java/com/alexjamesmalcolm/randomprogrammingpairs/PairGenerator.java
@@ -2,13 +2,27 @@ package com.alexjamesmalcolm.randomprogrammingpairs;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Random;
 
 public class PairGenerator {
 
-	private ArrayList<String> classmates;
+	private ArrayList<String> classmates = new ArrayList<>();
 
-	public PairGenerator(Collection<String> classmates) {
-		this.classmates = (ArrayList<String>) classmates;
+	public PairGenerator(ArrayList<String> classmates, boolean isRandom) {
+		if (isRandom) {
+			Random rng = new Random();
+			for(int i = 0; i < classmates.size(); i++) {
+				String classmate = classmates.get(i);
+				boolean coinFlip = rng.nextBoolean();
+				if(coinFlip) {
+					this.classmates.add(0, classmate);
+				} else {
+					this.classmates.add(classmate);
+				}
+			}
+		} else {
+			this.classmates = (ArrayList<String>) classmates;
+		}
 	}
 
 	public Collection<Collection<String>> getPairs() {
@@ -17,7 +31,7 @@ public class PairGenerator {
 			Collection<String> pair = new ArrayList<>();
 			pair.add(classmates.get(i));
 			pair.add(classmates.get(i + 1));
-			if(classmates.size() - i == 3) {
+			if (classmates.size() - i == 3) {
 				pair.add(classmates.get(i + 2));
 				i++;
 			}
--- src/test/java/com/alexjamesmalcolm/randomprogrammingpairs/PairGeneratorTest.java
+++ src/test/java/com/alexjamesmalcolm/randomprogrammingpairs/PairGeneratorTest.java
@@ -1,58 +1,62 @@
 package com.alexjamesmalcolm.randomprogrammingpairs;
 
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertThat;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 
 public class PairGeneratorTest {
-	
-	private Collection<String> classmates;
+
+	private ArrayList<String> classmates;
 
 	@Before
 	public void setup() {
 		classmates = new ArrayList<String>();
-//		classmates.add("Alex Richards");
-//		classmates.add("Jacob");
+		// classmates.add("Alex Richards");
+		// classmates.add("Jacob");
 	}
-	
+
 	@Test
 	public void shouldReturnCollectionOfPairs() {
 		classmates.add("Alex Richards");
 		classmates.add("Jacob");
-		PairGenerator underTest = new PairGenerator(classmates);
+		PairGenerator underTest = new PairGenerator(classmates, false);
 		Collection<Collection<String>> pairs = underTest.getPairs();
 		assertThat(pairs.isEmpty(), is(false));
 	}
-	
+
+	@SuppressWarnings("unchecked")
 	@Test
 	public void shouldReturnAlexAndRyanPair() {
 		classmates.add("Alex Malcolm");
 		classmates.add("Ryan");
-		PairGenerator underTest = new PairGenerator(classmates);
+		PairGenerator underTest = new PairGenerator(classmates, false);
 		Collection<String> pair = new ArrayList<>();
 		pair.add("Alex Malcolm");
 		pair.add("Ryan");
 		Collection<Collection<String>> pairs = underTest.getPairs();
 		assertThat(pairs, containsInAnyOrder(pair));
 	}
-	
+
 	@Test
 	public void shouldBeHalfTheLengthOfTheNumberOfClassmates() {
 		classmates.add("Alex Richards");
 		classmates.add("Jacob");
 		classmates.add("Alex Malcolm");
 		classmates.add("Ryan");
-		PairGenerator underTest = new PairGenerator(classmates);
+		PairGenerator underTest = new PairGenerator(classmates, false);
 		Collection<Collection<String>> pairs = underTest.getPairs();
 		assertThat(pairs.size(), is(2));
 	}
-	
+
 	@Test
 	public void shouldHaveTheStooges() {
 		classmates.add("Alex Richards");
@@ -60,8 +64,30 @@ public class PairGeneratorTest {
 		classmates.add("Alex Malcolm");
 		classmates.add("Ryan");
 		classmates.add("Susan");
-		PairGenerator underTest = new PairGenerator(classmates);
+		PairGenerator underTest = new PairGenerator(classmates, false);
 		Collection<Collection<String>> pairs = underTest.getPairs();
 		assertThat(pairs.size(), is(2));
+	}
+
+	@Test
+	public void shouldHaveRandomOrder() {
+		int count = 0;
+		int iterations = 100;
+		for (int i = 0; i < iterations; i++) {
+			classmates.add("Alex Richards");
+			classmates.add("Jacob");
+			classmates.add("Alex Malcolm");
+			classmates.add("Ryan");
+			PairGenerator underTest = new PairGenerator(classmates, true);
+			Collection<Collection<String>> pairs = underTest.getPairs();
+			Collection<String> pair = new ArrayList<>();
+			pair.add("Alex Richards");
+			pair.add("Jacob");
+			if(pairs.contains(pair)) {
+				count ++;
+			}
+		}
+		assertThat(count, is(lessThan(iterations)));
+		assertThat(count, is(greaterThan(0)));
 	}
 }
