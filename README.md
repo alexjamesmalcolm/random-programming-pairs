e521def0925fa307717968d66f8b3b901a6b1339
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-02-26 10:47:07 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Random Programming Pairs
+This is a project made by Alex Malcolm while attending We Can Code IT. It's a Spring MVC website that creates all new random 
+pairs of programmers.
---
+++ src/main/java/com/alexjamesmalcolm/randomprogrammingpairs/RandomProgrammingPairsApplication.java
@@ -0,0 +1,12 @@
+package com.alexjamesmalcolm.randomprogrammingpairs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomProgrammingPairsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomProgrammingPairsApplication.class, args);
+	}
+}
