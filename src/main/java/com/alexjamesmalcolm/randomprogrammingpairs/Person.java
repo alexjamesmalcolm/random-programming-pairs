8312f85f7c11576af6edeb0fb6e8d4a65bfe7ca4
Alex Malcolm <alexjamesmalcolm@gmail.com>
2018-02-26 13:24:08 -0500

    shouldHaveNameAlex

---
+++ src/main/java/com/alexjamesmalcolm/randomprogrammingpairs/Person.java
@@ -0,0 +1,12 @@
+package com.alexjamesmalcolm.randomprogrammingpairs;
+
+public class Person {
+
+	public Person(String name) {
+	}
+
+	public String getName() {
+		return "Alex";
+	}
+
+}
---
+++ src/test/java/com/alexjamesmalcolm/randomprogrammingpairs/PersonTest.java
@@ -0,0 +1,17 @@
+package com.alexjamesmalcolm.randomprogrammingpairs;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class PersonTest {
+
+	@Test
+	public void shouldHaveNameAlex() {
+		String name = "Alex";
+		Person underTest = new Person(name);
+		String returnedName = underTest.getName();
+		assertThat(returnedName, is(name));
+	}
+}
